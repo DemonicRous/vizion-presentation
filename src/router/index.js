49a9010a0243a08a796a9b87c9fff8119f421d9f
DemonicRous <asashachernov@mail.ru>
2026-05-17 22:39:32 +0500

    fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// src/router/index.js
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // Список слайдов для навигации (используется в NavigationBar)
 export const slideNames = [
     'intro', 'problems', 'goals', 'architecture',
     'tasks', 'audits', 'scoring', 'efficiency', 'conclusion'
-  ]
+]
 
 // Функция для динамического импорта компонентов слайдов
 const loadSlide = (name) => {
-  // имя компонента: SlideIntro.vue, SlideProblems.vue и т.д.
-  return () => import(`../views/slides/Slide${capitalize(name)}.vue`)
+    // имя компонента: SlideIntro.vue, SlideProblems.vue и т.д.
+    return () => import(`../views/slides/Slide${capitalize(name)}.vue`)
 }
 
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1)
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/intro'
-  },
-  {
-    path: '/:slide',
-    name: 'slide',
-    component: () => import('../views/HomeView.vue'),
-    props: true,
-    beforeEnter: (to, from, next) => {
-      const slide = to.params.slide
-      if (slideNames.includes(slide)) {
-        next()
-      } else {
-        next('/intro')
-      }
+    {
+        path: '/',
+        redirect: '/intro'
+    },
+    {
+        path: '/:slide',
+        name: 'slide',
+        component: () => import('../views/HomeView.vue'),
+        props: true,
+        beforeEnter: (to, from, next) => {
+            const slide = to.params.slide
+            if (slideNames.includes(slide)) {
+                next()
+            } else {
+                next('/intro')
+            }
+        }
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        redirect: '/intro'
     }
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: '/intro'
-  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  scrollBehavior() {
-    return { top: 0, left: 0 }
-  }
+    // Используем хэш-роутинг + base из переменной окружения Vite
+    // BASE_URL при сборке будет равен "/vizion-presentation/"
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes,
+    scrollBehavior() {
+        return { top: 0, left: 0 }
+    }
 })
 
 export default router
